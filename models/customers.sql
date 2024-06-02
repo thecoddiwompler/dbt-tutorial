7a9c274a4569ace71f8f82a6c00f9222cbe2cbb9
Indrajeet Mishra <mindra673@gmail.com>
2024-06-03 00:22:29 +0530

    Added a new model, tests, and documented it (#2)

--- models/customers.sql
+++ models/customers.sql
@@ -1,29 +1,12 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
-
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from jaffle_shop.raw_customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from jaffle_shop.raw_orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
